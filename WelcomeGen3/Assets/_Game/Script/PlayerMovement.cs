eb6a66215ebebba805216af7af25e6b5f7bb2d11
zKay <ductung.ptit@gmail.com>
2025-11-15 04:27:26 +0700

    fix sang dung côroutine

--- WelcomeGen3/Assets/_Game/Script/PlayerMovement.cs
+++ WelcomeGen3/Assets/_Game/Script/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,6 +12,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] LayerMask GroundLayer;
     [SerializeField] float firstShoot = 10f;
     [SerializeField] float secondShoot = 5f;
+    [SerializeField] float reloadTime = 2f;
+    Coroutine coroutine; // Kiem soat nap dan
+    public bool CanShoot = true;
 
     private bool onGround = false;
     public int countShoot = 0;
@@ -28,28 +32,43 @@ public class PlayerMovement : MonoBehaviour
 
     void Movement()
     {
-        if (mouseAction.WasPressedThisFrame())
+        if (mouseAction.WasPressedThisFrame() && CanShoot)
         {
             Vector2 MousePosition = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             Vector2 direction = ((Vector2)transform.position - MousePosition).normalized;
             countShoot++;
-            if(countShoot == 0)
+            if(countShoot == 1)
             {
                 _rb.linearVelocity = direction * firstShoot;
+                coroutine = StartCoroutine(ReloadShotgun());
             }
-            else if(countShoot == 1)
+            else if(countShoot == 2)
             {
                 _rb.linearVelocity = direction * secondShoot;
+                if(coroutine != null)
+                {
+                    StopCoroutine(coroutine);
+                    coroutine = null;
+                }
+                CanShoot = false;
+                coroutine = StartCoroutine(ReloadShotgun());
             }
         }
-        if(onGround)
-        {
-            countShoot = 0;
-        }
+        //if(onGround)
+        //{
+        //    countShoot = 0;
+        //}
     }
 
     private void isGrounded()
     {
         onGround = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.02f), CapsuleDirection2D.Horizontal, 0, GroundLayer);
+    }
+
+    IEnumerator ReloadShotgun()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        countShoot = 0;
+        CanShoot = true;
     }
 }
